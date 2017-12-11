dce4b0962902166c2ebd915ad998530113f8e008
cscully-allison <cscully-allison@nevada.unr.edu>
2017-12-11 14:47:49 -0800

    Removing line chart

--- Client/QC-Dashboard/src/app/datavis-dashboard/datavis-dashboard.component.ts
+++ Client/QC-Dashboard/src/app/datavis-dashboard/datavis-dashboard.component.ts
@@ -15,59 +15,6 @@ export class DatavisDashboardComponent implements OnInit {
   private passedData = {};
   private dataHolder = [];
 
-  public lineChartData:Array<any> = [
-   {data: [], label: 'Series A'}
-  ];
-
-  public lineChartLabels:Array<any> = [];
-
-  public lineChartOptions:any = {
-   responsive: true
-  };
-
-  public lineChartColors:Array<any> = [
-   { // grey
-     backgroundColor: 'rgba(148,159,177,0.2)',
-     borderColor: 'rgba(148,159,177,1)',
-     pointBackgroundColor: 'rgba(148,159,177,1)',
-     pointBorderColor: '#fff',
-     pointHoverBackgroundColor: '#fff',
-     pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-   },
-   { // grey
-     backgroundColor: 'rgba(148,159,177,0.2)',
-     borderColor: 'rgba(148,159,177,1)',
-     pointBackgroundColor: 'rgba(148,159,177,1)',
-     pointBorderColor: '#fff',
-     pointHoverBackgroundColor: '#fff',
-     pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-   }
-  ];
-
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
-
-  public randomize():void {
-   let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-   for (let i = 0; i < this.lineChartData.length; i++) {
-     _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-     for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-       _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-     }
-   }
-   this.lineChartData = _lineChartData;
-  }
-
-  // events
-  public chartClicked(e:any):void {
-   console.log(e);
-  }
-
-  public chartHovered(e:any):void {
-   console.log(e);
-  }
-
-
   constructor(private route:ActivatedRoute, private http:HttpClient) { }
 
   ngOnInit() {
@@ -75,7 +22,6 @@ export class DatavisDashboardComponent implements OnInit {
     if(true){
       this.sub = this.route.params.subscribe( passedData => {
         this.passedData = passedData;
-
       });
     }
 
@@ -84,18 +30,12 @@ export class DatavisDashboardComponent implements OnInit {
            console.log(data)
            for(let measurement in data["Measurements"]){
               var measurementRef =  data["Measurements"][measurement];
-               this.lineChartData[0].data.push(+measurementRef["Value"]);
-               this.lineChartLabels.push(measurementRef["Time Stamp"])
            }
-
-           console.log(this.dataHolder);
-
-           console.log(this.lineChartData[0]);
     })
-}
+  }
 
-ngOnDestroy() {
+  ngOnDestroy() {
     this.sub.unsubscribe();
-}
+  }
 
 }

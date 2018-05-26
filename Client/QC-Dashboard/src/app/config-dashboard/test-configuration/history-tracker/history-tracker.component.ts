5a3a2940276aaa13817926a40341e3baf5f329af
cscully-allison <cscully-allison@nevada.unr.edu>
2018-05-25 17:40:53 -0700

    Untracked files for last commit

---
+++ Client/QC-Dashboard/src/app/config-dashboard/test-configuration/history-tracker/history-tracker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'history-tracker',
+  templateUrl: './history-tracker.component.html',
+  styleUrls: ['./history-tracker.component.css']
+})
+export class HistoryTrackerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

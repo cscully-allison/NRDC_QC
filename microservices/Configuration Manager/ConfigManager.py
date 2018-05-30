880831b895f0409e070557ff4dcc60cbaff313bb
root <root@asgard-thor.sensor.nevada.edu>
2018-05-29 17:07:06 -0700

    Service manager scripts are executable

--- microservices/Configuration Manager/ConfigManager.py
+++ microservices/Configuration Manager/ConfigManager.py
@@ -1,10 +1,10 @@
 import sys
 sys.path.append("..")
-sys.path.append("../classes/")
+sys.path.append("../../classes")
 from flask import Flask, jsonify, json, request
 from flask_cors import cross_origin
 import pyodbc
-import TestConfiguration from Configuration.py
+from Configuration import TestConfiguration
 
 app = Flask(__name__)
 
@@ -15,7 +15,7 @@ def index():
 
 	# Try
 	try:
-		testconfig = new TestConfiguration("config/tests.config")
+		testconfig = TestConfiguration("../config/tests.config")
 		
 		print(testconfig.TestParameters)
 		
